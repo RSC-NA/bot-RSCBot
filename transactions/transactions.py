20664708fcfc0e6c0e8df17fe5e82ce780aee030
monty <monty@montys-mbp.myfiosgateway.com>
2023-08-17 12:57:30 -0400

    Ignore players with no teams.

--- transactions/transactions.py
+++ transactions/transactions.py
@@ -101,7 +101,10 @@ class Transactions(commands.Cog):
                     add_roles = [league_role, fa_role]
 
                 # get team/franchise info before role removal
-                team = (await self.team_manager_cog.teams_for_user(ctx, member))[0]
+                team = (await self.team_manager_cog.teams_for_user(ctx, member))
+                if len(team) == 0:
+                    continue
+
                 # gm_name = self.team_manager_cog._get_gm_name(franchise_role)
                 # franchise_name = self.team_manager_cog.get_franchise_name_from_role(franchise_role)
                 # gm: discord.Member = self.team_manager_cog._find_member_by_name(ctx, gm_name)
